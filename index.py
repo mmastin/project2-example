8cbf9ca17e4c57f0163b981fc342ddb097114cdf
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-06-26 12:36:44 -0400

    Updating for Census

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from tabs import intro, predict, explain, evaluate
 style = {'maxWidth': '960px', 'margin': 'auto'}
 
 app.layout = html.Div([
-    dcc.Markdown('# Lending Club interest rates'),
+    dcc.Markdown('# 2017 Census Tract Median Income Prediction'),
     dcc.Tabs(id='tabs', value='tab-intro', children=[
         dcc.Tab(label='Intro', value='tab-intro'), 
         dcc.Tab(label='Predict', value='tab-predict'), 
