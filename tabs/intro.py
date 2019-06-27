acf0f8948a10d61a54f20ea637856fc8e9ce3bd0
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-06-27 13:32:25 -0400

    Update intro.py

--- tabs/intro.py
+++ tabs/intro.py
@@ -12,6 +12,7 @@ With the 2020 Census on the horizon, I want to dive into the predictive powers o
 The 2017 ACS survey provides 36 high-level data points on the 3,142 counties and county-equivalents in the United States as well as 74,000 census tracts, with an average population of 4,400. With this information I trained regression models to predict Median Income for a given area, primarily using Random Forest and XGBoost Regressor.
 
 To make your own predictions, there are 6 sliders in the 'Predict' tab to control for a few of the most predictive features: Unemployment Rate, Poverty Rate, Professional Employment (% in management, business, science or arts jobs), Services Employment (% in service jobs), Production Employment (% in production or transportation jobs), and Average Commute Time.
+
 """),
           
 html.Img(src='/assets/1950censusform.jpg', style={'width': '100%'})]
