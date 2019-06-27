7ce8bbff59a6d4b626ea8cf68c051a59d3788a12
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-06-27 18:24:23 -0400

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
-from app import app
+from app import app, server
 from tabs import intro, predict, explain, evaluate
 
 style = {'maxWidth': '960px', 'margin': 'auto'}
