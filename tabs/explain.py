efe927fdd89fe3971a64c7bc237254bfc941f0e6
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-06-27 13:51:25 -0400

    Update explain.py

--- tabs/explain.py
+++ tabs/explain.py
@@ -4,10 +4,15 @@ import dash_html_components as html
 
 from app import app
 
-layout = dcc.Markdown("""
+layout = [dcc.Markdown("""
 ### Explain
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit ornare dolor in venenatis. Nunc dapibus elementum ligula, sed pharetra turpis tempor vel. Pellentesque dui tellus, euismod vel libero in, volutpat pretium felis. Sed sit amet purus et tellus tincidunt finibus. Integer augue odio, pharetra at placerat ut, venenatis nec erat. Integer eu malesuada nisl. Nam vestibulum, elit quis pharetra luctus, massa nulla cursus augue, in tristique mi ipsum id nisi. Fusce fringilla tortor elit, quis lacinia est molestie id. Nullam consectetur elit at porta faucibus. In feugiat, mauris ut dignissim ultrices, odio ex viverra ante, ac pulvinar libero mauris facilisis diam. Fusce at efficitur urna, in varius nisl. Pellentesque lectus odio, pellentesque sed lacinia eu, placerat et mauris. Sed id mi at dui aliquet dapibus non nec justo.
+An XGBoost Regressor model with 32 features and Ordinal Encoding most accurately predicted the Median Income of census tracts, with a Root Mean Squared Logarithmic Error of 0.167. I utilized a train dataframe of 58,174 tracts and a test dataframe of 14,544 tracts.
 
-Phasellus sodales vehicula nisi sit amet tincidunt. Integer mattis, lacus vitae tempor congue, tellus nulla congue lectus, id facilisis enim nunc eu arcu. Suspendisse vehicula metus non urna congue, ac fringilla dolor venenatis. Nulla facilisi. Etiam ornare ipsum id massa dictum, sit amet porttitor neque vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a purus vulputate leo egestas consectetur in nec augue.
-""")
+The interactive prediction model is a simplified version based on some of the most important features.
+
+"""),
+          
+html.Img(src='/assets/model.png', style ={'width':'100%'}),
+          
+html.Img(src='/assets/feature_importance.png', style={'width':'100%'})]
