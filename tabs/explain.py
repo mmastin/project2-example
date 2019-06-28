39a61bdd97510a21e6b0257b2a7873278e890809
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-06-28 10:03:26 -0400

    Update explain.py

--- tabs/explain.py
+++ tabs/explain.py
@@ -7,12 +7,12 @@ from app import app
 layout = [dcc.Markdown("""
 ### Explain
 
-An XGBoost Regressor model with 32 features and Ordinal Encoding most accurately predicted the Median Income of census tracts, with a Root Mean Squared Logarithmic Error of 0.167. I utilized a train dataframe of 58,174 tracts and a test dataframe of 14,544 tracts.
+An XGBoost Regressor model with 32 features and Ordinal Encoding most accurately predicted the Median Income of census tracts, with a Root Mean Squared Logarithmic Error of 0.167. I utilized a training dataframe of 58,174 tracts and a testing dataframe of 14,544 tracts.
 
 The interactive prediction model is a simplified version based on some of the most important features.
 
 """),
           
-html.Img(src='/assets/model.png', style ={'width':'100%'}),
+html.Img(src='/assets/model.png', style ={'width':'80%'}),
           
-html.Img(src='/assets/feature_importance.png', style={'width':'100%'})]
+html.Img(src='/assets/feature_importance.png', style={'width':'80%'})]
