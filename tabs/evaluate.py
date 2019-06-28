595b0ab7b1a2f1cc5db5e6084700b0830125f6e7
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-06-28 09:54:34 -0400

    Update evaluate.py

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -9,6 +9,10 @@ layout = [dcc.Markdown("""
 
 The distribution of predictions closely matches the true distribution of incomes, with slight overpredictions around the median and underpredictions of the few high income outliers.
 
+Unsurprisingly, median income of an area is highly correlated with unemployment and poverty rates. High levels of Professional and Services employment greatly boost incomes. 
+
+The model predicts highest incomes for areas with an average commute time of 36 minutes, presumably reflecting the prosperity of suburbs compared to urban and rural communities.
+
 """),
 
 html.Img(src='/assets/randomforest.png', style={'width': '100%'})]       
