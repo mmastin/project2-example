8b75728d906a12f2d0bb464b4ab7557e8973c07a
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-06-28 12:38:07 -0400

    Update intro.py

--- tabs/intro.py
+++ tabs/intro.py
@@ -9,7 +9,7 @@ layout = [dcc.Markdown("""
 
 With the 2020 Census on the horizon, I want to dive into the predictive powers of past data gathering efforts. While the Census is an enormous effort employing hundreds of thousands of people and projected to cost $15.6 billion next year, the American Community Survey collects yearly information on a smaller scale.
 
-The 2017 American Community Survey provides 36 high-level data points on the 3,142 counties and county-equivalents in the United States as well as 74,000 census tracts, which have an average population of 4,400. With this information I trained regression models to predict Median Income for a given area, primarily using Random Forest and XGBoost Regressor.
+The 2017 American Community Survey provides 36 high-level data points on the 3,142 counties and county-equivalents in the United States as well as 74,000 census tracts, which have an average population of 4,400. With this information I trained regression models to predict Median Income for a given area, primarily using Random Forest and XGBoost.
 
 To make your own predictions, there are six sliders in the 'Predict' tab to control for a few of the most predictive features: Unemployment Rate, Poverty Rate, Professional Employment (% in management, business, science or arts jobs), Services Employment (% in service jobs), Production Employment (% in production or transportation jobs), and Average Commute Time.
 
